f20f9e8ff242ffe1aa78c864d8c35a018cb76e42
Will Jallet <wilson.jallet@gmail.com>
2017-11-17 00:48:54 +0100

    Faster better stronger version

--- src/StableMatching.java
+++ src/StableMatching.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StableMatching implements StableMatchingInterface {
 
@@ -9,10 +11,31 @@ public class StableMatching implements StableMatchingInterface {
         // result array to be returned
         int[][] res = new int[m][w];
 
-        if (m==0 || w==0) return res;
+        if (m==0 || w==0) {
+            return res;
+        } else if (m==w) {
+            int n = 0;
+            for(int l: menGroupCount) {
+                n += l;
+            }
+            if (n==w) return backupAlgorithm(menGroupCount,womenGroupCount,menPrefs,womenPrefs);
+        }
 
-        // rejectedBy[i][j] is true whenever the j-th group of women has rejected the men in group i
-        boolean[][] rejectedBy = new boolean[m][w];
+        // nextIndex[i][j] is true whenever the j-th group of women has rejected the men in group i
+        int[] nextIndex = new int[m];
+
+        int[][] womenPrefsRankings = new int[w][m];
+        for(int j=0; j<w;j++) {
+            for(int i=0; i<m; i++) {
+                womenPrefsRankings[j][womenPrefs[j][i]] = i;
+            }
+        }
+
+        // current worst matchings
+        int[] worstMatch = new int[w];
+        for(int j=0; j<w; j++) {
+            worstMatch[j] = -1;
+        }
 
         // number of single men; we initialise at the biggest number
         int i = maxOfArray(menGroupCount);
@@ -27,20 +50,19 @@ public class StableMatching implements StableMatchingInterface {
         System.out.print(Arrays.toString(womenGroupCount));
         System.out.print(", ");
         */
-
         // number of single women
         int freeWomen;
 
         while (freeMen > 0) {
             /*
             System.out.print("seen=");
-            System.out.print(Arrays.deepToString(rejectedBy));
+            System.out.print(Arrays.toString(nextIndex));
             System.out.print(", ");
             */
 
             // get the group of women preferred by men in group i that hasn't rejected them yet
             // j is the number of this group
-            int j = firstSuchThat(menPrefs[i],rejectedBy[i]);
+            int j = menPrefs[i][nextIndex[i]];
             /*
             System.out.print("j=");
             System.out.print(j);
@@ -65,6 +87,14 @@ public class StableMatching implements StableMatchingInterface {
             // now, if there are free women, set them up with as many of the free men in group i as possible
             if (freeWomen > 0) {
                 res[i][j] += numToSwitch;
+
+                // now we check if our new men from group i are the worst
+                int currentWorstMatch = worstMatch[j];
+                if (currentWorstMatch == -1) {
+                    worstMatch[j] = i;
+                } else if (womenPrefsRankings[j][i] > womenPrefsRankings[j][currentWorstMatch]) {
+                    worstMatch[j] = i;
+                }
                 menGroupCount[i] -= numToSwitch;
                 womenGroupCount[j] -= numToSwitch;
 
@@ -73,14 +103,19 @@ public class StableMatching implements StableMatchingInterface {
                 // already engaged women have the priority over the singles
                 // find the least preferred group of men any woman in group j is currently engaged to
                 // rivalIndex is the position of this group in the group of women's preference list
+                /*
                 int rivalIndex = 0;
-                for(int k = 0; k < m; k++) {
+                for(int k = m-1; k >= 0; k--) {
                     if(res[womenPrefs[j][k]][j] > 0) {
                         rivalIndex = k;
+                        break;
                     }
                 }
-                int rival = womenPrefs[j][rivalIndex];
+                */
+                int rival = worstMatch[j];
 
+                // int rival = worstMatch[j];
+                int rivalIndex = womenPrefsRankings[j][rival];
                 /*
                 System.out.print("riv=");
                 System.out.print(rival);;
@@ -91,7 +126,7 @@ public class StableMatching implements StableMatchingInterface {
 
                 // now test the rival group against our current contender:
                 // if its index is farther than i's index, then it is not optimal for the women
-                int iIndex = searchFor(i, womenPrefs[j]);
+                int iIndex = womenPrefsRankings[j][i];
                 if (rivalIndex > iIndex) {
                 /* if the rival group is not as good as our current one,
                 make as many ladies engaged to them as possible switch */
@@ -104,11 +139,23 @@ public class StableMatching implements StableMatchingInterface {
 
                     menGroupCount[rival] += minimum(freeMen,occWomen);
                     menGroupCount[i] -= minimum(freeMen,occWomen);
-                    rejectedBy[rival][j] = true;
 
                     res[rival][j] -= minimum(freeMen,occWomen);
                     res[i][j] += minimum(freeMen,occWomen);
-                } else { rejectedBy[i][j] = true; }
+
+                    // if all the men from the rival group have been kicked to the curb
+                    // then we have to find the new worst match
+                    if (res[rival][j] == 0) {
+                        int indNewWorst = 0;
+                        for(int k = m-1; k >= 0; k--) {
+                            if(res[womenPrefs[j][k]][j] > 0) {
+                                indNewWorst = k;
+                                break;
+                            }
+                        }
+                        worstMatch[j] = womenPrefs[j][indNewWorst];
+                    }
+                } else { nextIndex[i]++; }
                 // System.out.print("),");
 
             }
@@ -124,8 +171,62 @@ public class StableMatching implements StableMatchingInterface {
             System.out.print(Arrays.toString(womenGroupCount));
             System.out.print(", ");
             */
+
             freeMen = menGroupCount[i];
         }
+        return res;
+    }
+
+    private int[][] backupAlgorithm(int[] menGroupCount, int[] womenGroupCount, int[][] menPrefs,int[][] womenPrefs) {
+        int n = menPrefs.length;
+        int[][] res = new int[n][n];
+
+        int[] match = new int[n];
+
+        int[] nextIndex = new int[n];
+
+        LinkedList<Integer> freeMen = new LinkedList<>();
+        for(int k = 0; k < n; k++) {
+            freeMen.add(k);
+        }
+
+        int[][] womenPrefsRankings = new int[n][n];
+        for(int j=0; j<n;j++) {
+            for(int i=0; i<n; i++) {
+                womenPrefsRankings[j][womenPrefs[j][i]] = i;
+            }
+        }
+
+        int i;
+        while (!freeMen.isEmpty()) {
+            i = freeMen.getFirst();
+            int j = menPrefs[i][nextIndex[i]];
+
+            if (womenGroupCount[j] > 0) {
+                womenGroupCount[j] = 0;
+                match[j] = i;
+                freeMen.poll();
+                res[i][j] = 1;
+            } else {
+                int rival = match[j];
+
+                int rivalIndex = womenPrefsRankings[j][rival];
+                int iIndex = womenPrefsRankings[j][i];
+
+                if (rivalIndex > iIndex) {
+                    res[i][j] = 1;
+                    res[rival][j] = 0;
+                    match[j] = i;
+                    freeMen.poll();
+                    freeMen.add(rival);
+                } else {
+                    nextIndex[i]++;
+                }
+            }
+
+        }
+
+
         return res;
     }
 
@@ -136,29 +237,9 @@ public class StableMatching implements StableMatchingInterface {
         return ind;
     }
 
-    /*
-     * Returns the first datapoint of array data that verifies the
-     * contraint constr
-     */
-    private int firstSuchThat(int[] data, boolean[] constr) {
-        for(int j=0; j < constr.length; j++) {
-            if (!constr[data[j]]) return data[j];
-        }
-        return constr.length-1;
-    }
-
     private int minimum(int x,int y) {
         if (x <= y) {
             return x;
         } else return y;
-    }
-
-    private int searchFor(int i,int[] data) {
-        for(int j=0; j<data.length; j++) {
-            if (data[j]==i) {
-                return j;
-            }
-        }
-        return -1;
     }
 }
